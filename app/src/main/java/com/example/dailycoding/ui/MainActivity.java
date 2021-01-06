fe1c457f2836af0c60f03b1996c4ecd2cb4c516b
yesaeop lee <yeseoplee94@gmail.com>
2021-01-06 15:14:50 +0900

    base

---
+++ app/src/main/java/com/example/dailycoding/ui/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.dailycoding.ui;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.example.dailycoding.R;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageButton btnMenu;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+        setListener();
+    }
+
+    private void init(){
+        btnMenu = findViewById(R.id.main_imageview_menu);
+        drawerLayout = findViewById(R.id.main_drawerlayout);
+        navigationView = findViewById(R.id.main_navigation);
+        navigationView.setItemIconTintList(null);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.add(R.id.main_framelayout, new HomeFragment());
+        fragmentTransaction.commit();
+    }
+
+    private void setListener(){
+
+        btnMenu.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.drawer_home:
+                        Toast.makeText(getApplicationContext(), "test", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
+
+    }
+
+}

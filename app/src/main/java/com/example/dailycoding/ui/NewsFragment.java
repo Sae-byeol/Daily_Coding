acbc4ff2943c5690796a79b6c09e8dd77141070b
Sae-byeol <toquf0424@naver.com>
2021-01-08 21:21:07 +0900

    commit2

--- app/src/main/java/com/example/dailycoding/ui/NewsFragment.java
+++ app/src/main/java/com/example/dailycoding/ui/NewsFragment.java
@@ -13,25 +13,34 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.dailycoding.R;
 
 public class NewsFragment extends Fragment {
+    private RecyclerView recyclerView,recyclerView2;
+    private RecyclerView.Adapter mAdapter;
+    private String[] mDataset={"news 1","news 2"};
 
     public static NewsFragment newInstance() {
         return new NewsFragment();
     }
 
-
-    private RecyclerView recyclerView;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView=(View)inflater.inflate(R.layout.fragment_news, container, false);
+
         recyclerView=(RecyclerView)rootView.findViewById(R.id.recyclerView1);
         LinearLayoutManager horizontalLayoutManager=new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false);
-
+        recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(horizontalLayoutManager);
+        mAdapter=new NewsAdapter(getActivity(),mDataset);
+        recyclerView.setAdapter(mAdapter);
+
+        recyclerView2 = (RecyclerView) rootView.findViewById(R.id.recyclerView2);
+        LinearLayoutManager horizontalLayoutManager2=new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false);
+        recyclerView2.setHasFixedSize(true);
+        recyclerView2.setLayoutManager(horizontalLayoutManager2);
+        mAdapter=new NewsAdapter(getActivity(),mDataset);
+        recyclerView2.setAdapter(mAdapter);
 
         return rootView;
-
     }
 
 }

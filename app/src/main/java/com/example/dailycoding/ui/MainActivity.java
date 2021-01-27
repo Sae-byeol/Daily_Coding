e8a00024ed68e93a4d5bc72161de6d224dfb5592
Sae-byeol <toquf0424@naver.com>
2021-01-28 00:04:25 +0900

    크기 다 맞춤

--- app/src/main/java/com/example/dailycoding/ui/MainActivity.java
+++ app/src/main/java/com/example/dailycoding/ui/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.LayoutInflater;

b802e2a0eebf5e91db7709f7aa4ffdb58712e47f
Jaejuna <anayana9988@gmail.com>
2021-02-01 17:30:43 +0900

    profile activity & splash screen layout

---
+++ app/src/main/java/com/example/dailycoding/ui/ProfileActivity.java
@@ -0,0 +1,17 @@
+package com.example.dailycoding.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.dailycoding.R;
+import com.example.dailycoding.util.BaseActivity;
+
+public class ProfileActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
---
+++ app/src/main/java/com/example/dailycoding/ui/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.example.dailycoding.ui;
+
+import android.os.Bundle;
+
+import com.example.dailycoding.R;
+import com.example.dailycoding.util.BaseActivity;
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+    }
+}

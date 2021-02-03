ed62cbb86378d02813f22274d50692777205d80e
Sae-byeol <toquf0424@naver.com>
2021-02-03 20:04:30 +0900

    스크롤바 해결

--- app/src/main/java/com/example/dailycoding/ui/MainActivity.java
+++ app/src/main/java/com/example/dailycoding/ui/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends BaseActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this, AgreeCheckActivity.class);
+                Intent intent=new Intent(MainActivity.this, ProfileActivity.class);
                 startActivity(intent);
             }
         });
